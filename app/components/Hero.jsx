51074bce25395a7a068ca5341c34919b9a17cfca
Dev-webp <srichain18@gmail.com>
2024-12-04 19:09:14 +0530

    3

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 // import Link from "next/link";
 import { motion } from "framer-motion";
 import { containerVariants, desVariants } from "../animation";  
